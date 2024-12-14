4c38543b6cb5cf915a15f2eabde54ce2c94d797c
Dostonbek <dostonbek200gmail.com>
2024-12-14 22:23:18 +0500

    for loop

---
+++ app.js
@@ -0,0 +1,83 @@
+// let son = +prompt('soni kiriting')
+// for(let i = 1; i <=10; i++){
+//    console.log(`${i} x ${son} = ${i * son}`)
+// }
+// let harg = 'hvjvhbhjhhhhhhhhhhhhhhh'
+// for (let i = 0; i<harg.length;i++ ){
+//    if (harg[i].toLowerCase() == 'h'){
+//       console.log(1)
+//    }
+// }
+
+// let son = 3
+// let answer = ['tinch','Atlantika' ,'Shimoliy muz']
+// for (let i = 65; i < 65 + son; i++){
+//    console.log(String.fromCharCode(i).toLowerCase(), answer[i - 65])
+// }
+// 1 uy ishi
+let k = 10
+let n = 5 
+for (let i = 0; i< n; i++){
+   console.log(k)
+}
+// 2
+// let n = 8
+// for (let i = 1; i < 2*n-1; i++){
+//    console.log(i**2)
+// }
+// 3
+// let konfet = 3500
+// let sum = [ 0.1, 0.2,0.3,0.4,0.5,0.6,,0.8, 0.9, 1]
+
+// for (let i = 0.1; i <=1; i+=0.1){
+//    console.log(`${i} x ${konfet} = ${i*konfet}`)
+// }
+// 4
+// let n = 8
+// s = 1
+// for (let i = n; i < 2*n; i++){
+//    s +=i**2
+//    console.log(`yig'indi ${s}`)
+// }
+// 5
+// let n = 5
+// let s= 0
+// for (let i = 1; i<= n; i++ ){
+//    s += i**(n - i + 1)
+//    console.log(s)
+// }
+// 6
+// let a = 5
+// let b= 9
+// for ( let i = 5; i <= 9; i++){
+   
+//    console.log((`${i}`).repeat(i))
+// }
+// 7
+//   let son = 12
+//   for (let i = 0; i <= son; i++){
+//    if(son % i === 0){
+//       console.log(i)
+//    }
+//   }
+
+// 8
+// let a = 8
+// let b = 20
+// for (let i =8; i <= b; i++){
+//    console.log(i)
+// }
+// let a = 5
+// let b = 20
+// for ( let i = 5; i < b; i--){
+//    if(i > a && b < i ){
+//       console.log(i)
+//    }
+// }
+
+// let son = 3505
+// for (let i = 1.2; i <= 2; i+=0.2){
+//    console.log(`${i*son}`)
+// }
+
+
